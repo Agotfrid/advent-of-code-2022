e2ac20acc4c1c43125f82c13b6b0af40a6e68e74
Alex Gotfrid <alexander.gotfrid@boldcommerce.com>
2023-05-21 15:05:06 -0500

    update folders

--- day_1/main.go
+++ day_1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -17,36 +18,73 @@ func readFile(filename string) ([]string, error) {
 	return units, nil
 }
 
+func findTotalCalories(unit string) (int, error) {
+	lines := strings.Split(strings.TrimSpace(unit), "\n")
+	total := 0
+	for _, line := range lines {
+		value, err := strconv.Atoi(line)
+		if err != nil {
+			return 0, fmt.Errorf("Error converting values to int: %w", err)
+		}
+		total += value
+	}
+
+	return total, nil
+}
+
+// func findHighestSum(sums []int) (int, int) {
+// 	highestSum := 0
+// 	unit := 0
+// 	for i, sum := range sums {
+// 		if sum > highestSum {
+// 			highestSum = sum
+// 			unit += i
+// 		}
+// 	}
+// 	return unit, highestSum
+// }
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide a filename as an argument.")
 		os.Exit(1)
 	}
-	filename := os.Args[1]
 
-	maxSum := 0
-	maxUnit := 0
+	if len(os.Args) < 3 {
+		fmt.Println("Please provide number of top elves to assess")
+		os.Exit(1)
+	}
+	filename := os.Args[1]
+	topElves, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Error converting values to int", err)
+		os.Exit(1)
+	}
+
 	units, err := readFile(filename)
 	if err != nil {
 		panic("error reading file")
 	}
 
-	for i, unit := range units {
-		lines := strings.Split(strings.TrimSpace(unit), "\n")
-		sum := 0
-
-		for _, line := range lines {
-			value, err := strconv.Atoi(line)
-			if err != nil {
-				fmt.Println("Error converting values to int:", err)
-			}
-			sum += value
+	var sums []int
+	for _, unit := range units {
+		sum, err := findTotalCalories(unit)
+		if err != nil {
+			fmt.Println("Error caclulating total number of calories", err)
+			os.Exit(1)
 		}
-
-		if sum > maxSum {
-			maxSum = sum
-			maxUnit += i
-		}
+		sums = append(sums, sum)
 	}
-	fmt.Printf("Elf #%d is carrying a total of %d calories\n", maxUnit, maxSum)
+	sort.Ints(sums)
+
+	topTotal := sums[len(sums)-topElves:]
+
+	var total int
+	for _, number := range topTotal {
+		total += number
+	}
+	fmt.Printf("Top %d Elves are carrying %d calories\n", topElves, total)
+
+	// unit, highestSum := findHighestSum(sums)
+	// fmt.Printf("Elf #%d is carrying a total of %d calories\n", unit, highestSum)
 }
